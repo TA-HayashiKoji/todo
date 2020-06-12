8edd69dd84107e924d2d3bb838ffa97227666bca
kojihayashi <k-hayashi@tri-ash.com>
2020-06-12 13:16:14 +0900

    入門Laravelチュートリアル (10) エラーハンドリング

--- routes/web.php
+++ routes/web.php
@@ -11,20 +11,21 @@
 |
 */
 
-Route::group(['middleware' => 'auth'], function() {
+Route::GROUP(['middleware' => 'auth'], function() {
     Route::GET('/', 'HomeController@index')->name('home');
-
-    Route::GET('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 
     Route::GET('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
     Route::POST('/folders/create', 'FolderController@create');
 
-    Route::GET('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
-    Route::POST('/folders/{id}/tasks/create', 'TaskController@create');
+    Route::GROUP(['middleware' => 'can:view,folder'], function() {
+        Route::GET('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
 
-    Route::GET('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
-    Route::POST('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+        Route::GET('/folders/{folder}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+        Route::POST('/folders/{folder}/tasks/create', 'TaskController@create');
 
+        Route::GET('/folders/{folder}/tasks/{task}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+        Route::POST('/folders/{folder}/tasks/{task}/edit', 'TaskController@edit');
+    });
 });
 
 Auth::routes();
